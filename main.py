162a4488db3d84074a5be7c6cbd8a65b3782cdb7
Mr4d <sford@sd43.bc.ca>
2022-09-02 18:55:54 +0000

    Dict

---
+++ main.py
@@ -0,0 +1,33 @@
+#print("Hello Python 4D")
+
+#Varialbles
+hello_str = "Hello Python 4D"
+print(type(hello_str))
+hello_int = 21
+print(type(hello_int))
+hello_float = 3.14
+print(type(hello_float))
+hello_bool = True
+print(type(hello_bool))
+hello_tuple = (21, 32, 54)
+print(type(hello_tuple))
+hello_list = ["hello","this","is", "a", "list", ":)"]
+hello_dict = {"first_name":"Vlady", "last_name": "Guerrro", "pos1": "First Base", "pos2": "DH"}
+print(type(hello_dict))
+
+#print(hello_list)
+
+#Lists are dynamic and can be defined "initiated empty" or "redefined empty"
+
+#hello_list = list()
+#print(hello_list)
+
+#print(hello_list[0], hello_list[1], hello_list[2], hello_list[3], hello_list[4], hello_list[-1]) 
+
+
+
+
+
+
+
+
